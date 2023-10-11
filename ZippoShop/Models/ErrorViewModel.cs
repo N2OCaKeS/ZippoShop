068dec3bb711a43574bb6980cdde48edcf65584b
n2o_cake <n2o.cake@gmail.com>
2023-10-11 15:48:55 +0300

    Добавьте файлы проекта.

---
+++ ZippoShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZippoShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
